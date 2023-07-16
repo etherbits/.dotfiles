b7317dacc51773d5f30ababc75bba071a244ae5e
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-07-16 13:14:00 +0400

    changing bash command location

---
+++ .config/bash_commands.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+export EDITOR="nvim"
+export VISUAL="nvim"
+export PATH="$HOME/.tmuxifier/bin:$PATH"
+
+# git quick commit
+function qc(){
+  git add -A
+  git commit -m "$1"
+}
+
+# git quick push
+function qp(){
+  git push origin $1
+}
+
+# git quick commit and push
+function qcp(){
+  git add -A
+  git commit -m "$1"
+  git push origin main
+}
+
+# open github profile
+function ghme(){
+  xdg-open https://github.com/etherbits
+}
+
+# cd to projects
+function cdp(){
+  cd ~/projects/$1
+}
+
+# open commands config
+function commandConfig() {
+  cd ~/bash-commands/
+  nvim ./bash-commands.sh
+}
+
+# open # open nvim config
+function nvimConfig() {
+  cd ~/.config/nvim/
+  nvim .
+}
+
+# open alacritty config
+function alacrittyConfig(){
+  cd ~/.config/alacritty/
+  nvim .
+}
