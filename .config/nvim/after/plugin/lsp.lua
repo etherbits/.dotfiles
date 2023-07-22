f7b74ea1ba994233faacb7f416333d14f5450824
etherbits <nika.qvrivishviliPC@gmail.com>
2023-07-21 22:28:33 -0800

    init

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,80 @@
+local lsp = require('lsp-zero').preset({})
+-- (Optional) Configure lua language server for neovim
+local lspconfig = require('lspconfig')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.emmet_ls.setup({
+    -- on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+})
+
+lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+
+
+local cmp = require("cmp")
+
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		-- { name = 'vsnip' }, -- For vsnip users.
+		{ name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+	['<Tab>'] = cmp.mapping.confirm({ select = true }),
+	['<C-l>'] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings
+})
+
+lsp.on_attach(function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+lsp.setup()
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,94 @@
+return { 'nvim-lua/popup.nvim', 'sudormrfbin/cheatsheet.nvim',
+  'numToStr/Navigator.nvim',
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+  },
+  "lukas-reineke/indent-blankline.nvim",
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require 'nordic'.load()
+    end
+  },
+  {
+    'filipdutescu/renamer.nvim',
+    branch = 'master'
+  },
+  'windwp/nvim-ts-autotag',
+  {
+    'olivercederborg/poimandres.nvim',
+    config = function()
+      require('poimandres').setup {
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      }
+    end
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  },
+  "rmehri01/onenord.nvim",
+  "folke/tokyonight.nvim",
+  "ful1e5/onedark.nvim",
+  "Alexis12119/nightly.nvim",
+  "rebelot/kanagawa.nvim",
+  { "catppuccin/nvim",             name = "catppuccin" },
+  { 'projekt0n/github-nvim-theme', version = 'v0.0.7' },
+  'nyoom-engineering/oxocarbon.nvim',
+  { "CRAG666/code_runner.nvim", config = true },
+  "ggandor/leap.nvim", "numToStr/Comment.nvim", "Pocco81/auto-save.nvim", "jiangmiao/auto-pairs",
+  "MunifTanjim/prettier.nvim",
+  "jose-elias-alvarez/null-ls.nvim",
+  "rafamadriz/friendly-snippets",
+  "VonHeikemen/lsp-zero.nvim",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+  'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer'
+, 'hrsh7th/cmp-path'
+, 'hrsh7th/cmp-cmdline'
+, 'hrsh7th/nvim-cmp',
+  { 'L3MON4D3/LuaSnip',         dependencies = { "rafamadriz/friendly-snippets" } },
+  'saadparwaiz1/cmp_luasnip',
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring', },
+    build = ":TSUpdate"
+  },
+  "nvim-treesitter/nvim-treesitter-context",
+  "nvim-lua/plenary.nvim",
+  {
+    "iamcco/markdown-preview.nvim",
+    config = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+  'jghauser/follow-md-links.nvim',
+  "folke/zen-mode.nvim",
+  {
+    "nvim-telescope/telescope.nvim", tag = "0.1.1" },
+  "ThePrimeagen/harpoon"
+}
