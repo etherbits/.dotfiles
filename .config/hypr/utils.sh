154eaef79d51f8472561284f7589717f7a995764
etherbits <nika.qvrivishviliPC@gmail.com>
2023-10-31 00:03:11 +0400

    timer works with some bugs

--- .config/hypr/utils.sh
+++ .config/hypr/utils.sh
@@ -7,7 +7,7 @@ screenshot(){
 
 setTimer(){
   source $HOME/.zshrc
-  timer 15:00 --waybar $HOME/status/timer; notify-send timer
+  timer "$(echo '05:00\n10:00\n15:00\n30:00\n45:00' | rofi -dmenu -lines 0 -p 'Set timer' )" --waybar $HOME/status/timer; notify-send '  Timer' 'Timer has finished' -u critical; play $HOME/audio/alarm-sound.wav repeat 4
 }
 
 if [[ "$1" == "--screenshot" ]]; then
