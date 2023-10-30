90b30e5977e558b09a79502a486c03e8443c1bdd
etherbits <nika.qvrivishviliPC@gmail.com>
2023-10-30 18:52:53 +0400

    moving timer status out file location

--- .config/hypr/utils.sh
+++ .config/hypr/utils.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+#!/bin/zsh
 
 screenshot(){
     border-bottom: 3px solid rgba(100, 114, 125, 0.5);
   grimblast copysave area "$HOME/screenshots/$(date +%d-%m-%y_%H-%M-%S)_screenshot.png"
 }
 
+setTimer(){
+  source $HOME/.zshrc
+  timer 15:00 --waybar $HOME/status/timer; notify-send timer
+}
+
 if [[ "$1" == "--screenshot" ]]; then
   screenshot
+elif [[ "$1" == "--timer" ]]; then
+  setTimer
 fi
--- .config/waybar/scripts/timer.sh
+++
@@ -1,3 +0,0 @@
-# zsh "./timer" 15:00
-
-"$HOME/projects/timer-cli/a.out" 15:00
