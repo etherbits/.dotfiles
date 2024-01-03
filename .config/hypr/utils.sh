2d1efd3c5ee056c80c9d19899ea706aab576e9ab
etherbits <nika.qvrivishviliPC@gmail.com>
2024-01-03 08:37:36 +0400

    lowering alarm volume

--- .config/hypr/utils.sh
+++ .config/hypr/utils.sh
@@ -7,7 +7,7 @@ screenshot(){
 
 setTimer(){
   source $HOME/.zshrc
-  timer "$(echo '05:00\n10:00\n15:00\n30:00\n45:00' | rofi -dmenu -lines 0 -p 'Set timer' )" --waybar $HOME/status/timer; notify-send '  Timer' 'Timer has finished' -u critical; play $HOME/audio/alarm-sound.wav repeat 4
+  timer "$(echo '05:00\n10:00\n15:00\n30:00\n45:00' | rofi -dmenu -lines 0 -p 'Set timer' )" --waybar $HOME/status/timer; notify-send '  Timer' 'Timer has finished' -u critical; play -v 0.4 $HOME/audio/alarm-sound.wav repeat 2
 }
 
 if [[ "$1" == "--screenshot" ]]; then
