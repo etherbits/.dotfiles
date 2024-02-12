c909ffc0554e3b1a8de607da7ccb7f2f0f1a0fac
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2024-02-12 09:37:11 +0400

    resuming

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -68,6 +68,23 @@ return {
     end,
   },
   "ThePrimeagen/harpoon",
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",   -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+    },
+  },
   "NvChad/nvim-colorizer.lua",
   {
     "nvim-lualine/lualine.nvim",
